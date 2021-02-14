2f34e0f0bdcc42ac75764abf7ccc80cbe9b2c24b
Jordan Egstad <jordanegstad@gmail.com>
2021-02-14 13:52:43 -0800

    feat: added netlify plugin

--- components/organisms/PrimaryNavigation.vue
+++ components/organisms/PrimaryNavigation.vue
@@ -4,6 +4,9 @@
       <li v-for="link in links" :key="link.title">
         <nuxt-link :to="{ path: link.route }">{{ link.title }}</nuxt-link>
       </li>
+      <li>
+        <nuxt-link :to="{ path: '/is/a/bitch' }">test</nuxt-link>
+      </li>
     </ul>
   </nav>
 </template>
