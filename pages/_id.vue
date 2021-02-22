6fdb8d59ef5f391bea83db1b39240c3fc267a09b
Jordan Egstad <jordanegstad@gmail.com>
2021-02-21 22:23:57 -0800

    chore: updated groq query

--- pages/_id.vue
+++ pages/_id.vue
@@ -15,7 +15,7 @@ import { groq } from '@nuxtjs/sanity'
 export default {
   async asyncData({ $sanity, params, isDev, error }) {
     const uid = params.id
-    const query = groq`*[_type == "page" && tabs.slug.current == "${uid}"][0]`
+    const query = groq`*[_type == "page" && content.slug.current == "${uid}"][0]`
     const data = await $sanity.fetch(query).then((res) => {
       if (!res) {
         error({ statusCode: 404, message: `Document '${uid}' doesn't exist` })
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="container">
     <h1>Hello World, it me</h1>
-    <pre>{{ document }}</pre>
+
+    <Slices :slices="document.content.slices"></Slices>
   </div>
 </template>
 
 <script>
 import { groq } from '@nuxtjs/sanity'
+import Slices from '@/components/templates/Slices'
 
 export default {
+  components: {
+    Slices
+  },
   asyncData({ $sanity }) {
-    const query = groq`{ "document": *[_type == "page" && content.title == "Index"][0] }`
+    const query = groq`{ "document": *[_type == "piece"][0] }`
 
     return $sanity.fetch(query)
-  },
-  head() {
-    return this.$setPageMetadata(this.document.content)
   }
+  // head() {
+  //   return this.$setPageMetadata(this.document.content)
+  // }
 }
 </script>
