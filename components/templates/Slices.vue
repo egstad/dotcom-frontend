e84a9f6f67ab6be97fa9c02526fbfbc366e40eaf
Jordan Egstad <jordanegstad@gmail.com>
2021-02-21 22:29:10 -0800

    feat: added slices teamplate

---
+++ components/templates/Slices.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="slices">
+    <template v-for="slice in slices">
+      <template v-if="slice._type === 'video'">
+        <Vid
+          :key="slice._key"
+          :alt="slice.alt"
+          :url="slice.url"
+          :config="slice.settings"
+        />
+      </template>
+    </template>
+  </section>
+</template>
+
+<script>
+import Vid from '@/components/atoms/Vid'
+
+export default {
+  components: {
+    Vid
+  },
+  props: {
+    slices: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
