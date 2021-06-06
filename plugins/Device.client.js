e1e348a15b4d58e6e050bc20ffac7d72d1997880
Egstad <jordanegstad@gmail.com>
2021-06-05 21:05:36 -0700

    feat: added device plugin

---
+++ plugins/Device.client.js
@@ -0,0 +1,57 @@
+import deviceInfo from '~/plugins/device/DeviceInfo'
+import windowResize from '~/plugins/device/WindowResize'
+import windowDimensions from '~/plugins/device/WindowDimensions'
+import windowScroll from '~/plugins/device/WindowScroll'
+import preferredTheme from '~/plugins/device/PreferredTheme'
+import preferredMotion from '~/plugins/device/PreferredMotion'
+
+const device = {
+  init() {
+    deviceInfo.init()
+    preferredTheme.init()
+    preferredMotion.init()
+    windowResize.init()
+    windowDimensions.init()
+    windowScroll.init()
+    // this.scrollHistory()
+  },
+
+  setDeviceClasses() {
+    // touch support
+    if (this.isTouch) {
+      document.documentElement.classList.add('device-is-touch')
+    }
+
+    // cursor support
+    if (this.isCursor) {
+      document.documentElement.classList.add('device-is-pointer')
+    }
+
+    // mobile device
+    if (this.isMobile) {
+      document.documentElement.classList.add('device-is-mobile')
+    }
+  },
+
+  // sets scroll restoration to manual from auto
+  // this is so that the browser doesn't jump the gun and
+  // change scroll position before the route has completed
+  scrollHistory() {
+    if ('scrollRestoration' in history) {
+      history.scrollRestoration = 'manual'
+    }
+  }
+}
+
+if (process.browser) {
+  window.onNuxtReady((app) => {
+    // run all of the above
+    device.init()
+    // tell nuxt that we're ready to rock
+    window.$nuxt.$emit('device::initialized')
+    // update on new page
+    // window.$nuxt.$on('page::mounted', device.getWindowDimensions)
+  })
+}
+
+export default device
